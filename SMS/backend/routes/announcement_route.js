990c6d6895ac382be2f6e619147bdec07be3e51f
vishwavegad <vegadvishwa17@gmail.com>
2025-02-15 11:32:00 +0530

    new announcement routes added

--- SMS/backend/routes/announcement_route.js
+++ SMS/backend/routes/announcement_route.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const Announcement = require("../models/announcements_model");
-const {getAllAnnouncements, createNewAnnouncement} = require("../controllers/announcement_controller");
+const {getAllAnnouncements, createNewAnnouncement, getAnnouncementById, updateAnnouncement, deleteAnnouncement} = require("../controllers/announcement_controller");
 const router = express.Router();
 
 router.route('/')
     .get(getAllAnnouncements)
     .post(createNewAnnouncement);
+
+router.route('/:id')
+    .get(getAnnouncementById)
+    .put(updateAnnouncement)
+    .delete(deleteAnnouncement)
 
 module.exports = router;
