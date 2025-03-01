3f90880db3209c565dd74a5957d17e35035377da
vishwavegad <vegadvishwa17@gmail.com>
2025-03-01 16:06:18 +0530

    signup route added

--- SMS/backend/models/user_model.js
+++ SMS/backend/models/user_model.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true
     },
     email: {
         type: String,
@@ -18,29 +20,22 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    profilePic: {
-        type: String,//URL of the profile picture
-    },
-    flatNum: {
+    role: {
         type: String,
-        required: true,
+        enum: ["user", "admin"],
+        default: "user"
     },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    contactNum: {
-        type: Number,
-        required: true,
-    },
-    payment: {
-        type: Number,
-        default: 0,
-    },
-    isOwner: {
-        type: Boolean,
-        default: true,
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 },{timestamps: true});
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password"))
+            return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
 
 module.exports = mongoose.model('User', userSchema);
