e31551d404a608bec40c326fde32e54deaff5f24
vishwavegad <vegadvishwa17@gmail.com>
2025-03-06 22:17:20 +0530

    complaint backend integrated

--- SMS/backend/models/complaints_model.js
+++ SMS/backend/models/complaints_model.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 
 const complaintsSchema = new mongoose.Schema({
     username: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        // type: String,
+        // type: mongoose.Schema.Types.ObjectId,
+        // ref: "User",
+        type: String,
         required: true,
         trim: true,
     },
     email: {
-        type: mongoose.Schema.Types.ObjectId,
+        // type: mongoose.Schema.Types.ObjectId,
         // ref: "User",
-        // type: String,
+        type: String,
         required: true,
         trim: true,
         // unique: true,
