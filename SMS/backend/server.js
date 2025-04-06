c1b0301ac2525e13dd48895ee69507f5e187192f
Aanshi2124 <aanshivadera@gmail.com>
2025-04-06 21:57:28 +0530

    visitor page backend

--- SMS/backend/server.js
+++ SMS/backend/server.js
@@ -1,43 +1,48 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // const userRouter = require('./routes/user_route');
- const loginRouter = require("./routes/login_route");  
+const loginRouter = require("./routes/login_route");
 const signUpRouter = require("./routes/signup_route");
 const announcementRouter = require("./routes/announcement_route");
 const maintenanceRouter = require("./routes/maintenance_route");
 const complaintRouter = require("./routes/complaint_route");
-const connectMongoDB = require('./config/connection');
+const visitorRouter = require("./routes/visitorsLog_route");
+const connectMongoDB = require("./config/connection");
 const PORT = 3000;
 
 const app = express();
 
 //to parse JSON requests
-app.use(express.json());  
+app.use(express.json());
 //for frontend communication
-app.use(cors());  
+app.use(cors());
 
-connectMongoDB('mongodb://127.0.0.1:27017/SMS')
-.then(() => console.log('MongoDB connection established'));
+connectMongoDB("mongodb://127.0.0.1:27017/SMS").then(() =>
+  console.log("MongoDB connection established")
+);
 
 app.use(express.urlencoded({ extended: false }));
 
 // app.use('/api/users', userRouter);
 
 //User Login
-app.use('/auth/login', loginRouter);
+app.use("/auth/login", loginRouter);
 
 //User SignUp
 app.use("/auth/signup", signUpRouter);
 
 //Announcement Route
-app.use('/api/announcements', announcementRouter);
+app.use("/api/announcements", announcementRouter);
 
 //Maintenance Route
-app.use('/api/maintenance', maintenanceRouter);
+app.use("/api/maintenance", maintenanceRouter);
 
 //Complaint Route
-app.use('/api/complaints',complaintRouter);
+app.use("/api/complaints", complaintRouter);
+
+//Visitor Route
+app.use("/api/visitorsLog", visitorRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
